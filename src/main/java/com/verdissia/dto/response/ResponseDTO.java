4d43329cd64d2d1092ce963e740822d2904ffff5
oumaf <afraoui.oumaima@gmail.com>
2026-01-24 16:35:32 +0100

    [VERDISSIA-4] Nettoyage partie keycloak

--- src/main/java/com/verdissia/dto/response/ResponseDTO.java
+++ src/main/java/com/verdissia/dto/response/ResponseDTO.java
@@ -1,12 +1,5 @@
 package com.verdissia.dto.response;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@EqualsAndHashCode
 public class ResponseDTO<T> implements IResponseDTO {
 
     private String status;
@@ -17,10 +10,108 @@ public class ResponseDTO<T> implements IResponseDTO {
 
     private CodeMessage info;
 
-    @Getter
-    @Setter
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public CodeMessage getError() {
+        return error;
+    }
+
+    public void setError(CodeMessage error) {
+        this.error = error;
+    }
+
+    public CodeMessage getInfo() {
+        return info;
+    }
+
+    public void setInfo(CodeMessage info) {
+        this.info = info;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ResponseDTO<?> that = (ResponseDTO<?>) o;
+        if (status != null ? !status.equals(that.status) : that.status != null) {
+            return false;
+        }
+        if (data != null ? !data.equals(that.data) : that.data != null) {
+            return false;
+        }
+        if (error != null ? !error.equals(that.error) : that.error != null) {
+            return false;
+        }
+        return info != null ? info.equals(that.info) : that.info == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = status != null ? status.hashCode() : 0;
+        result = 31 * result + (data != null ? data.hashCode() : 0);
+        result = 31 * result + (error != null ? error.hashCode() : 0);
+        result = 31 * result + (info != null ? info.hashCode() : 0);
+        return result;
+    }
+
     public static class CodeMessage {
         private String code;
         private String message;
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            CodeMessage that = (CodeMessage) o;
+            if (code != null ? !code.equals(that.code) : that.code != null) {
+                return false;
+            }
+            return message != null ? message.equals(that.message) : that.message == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = code != null ? code.hashCode() : 0;
+            result = 31 * result + (message != null ? message.hashCode() : 0);
+            return result;
+        }
     }
 }
